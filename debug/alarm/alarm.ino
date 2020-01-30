c939ee1afe93044018d77892ac2df211a71b754d
Tikhon <41113186+TikhonP@users.noreply.github.com>
2020-01-30 18:31:33 +0300

    Дебаг для сигнализации

---
+++ debug/alarm/alarm.ino
@@ -0,0 +1,48 @@
+#define ALARM_PIN 2 // Пин подключения сигналов после оптореле
+#define DELAY_SIGNALS 1000 // Задержка между сигналами в миллисекундах
+bool is_alrm = false;
+
+unsigned long nowTime; // Последнее время срабатывания сигнала
+int signals;
+
+
+int readCommand() {
+        int signalsCount = 1; // Количесво поступающих сигналов
+        while (true) {
+                if ((millis() - nowTime)>DELAY_SIGNALS) {
+                        if (digitalRead(ALARM_PIN)) {
+                                signalsCount = 0;
+                        }
+                        return signalsCount;
+                }
+                if (digitalRead(ALARM_PIN)) {
+                        nowTime = millis();
+                        signalsCount++;
+                }
+        }
+
+}
+
+void inter_alrm() {
+        is_alrm = true;
+}
+
+
+void setup() {
+        Serial.begin(9600); // Вывод
+        Serial.println("Out 9600");
+        // Для сигнализации
+        pinMode(ALARM_PIN, INPUT);
+        attachInterrupt(0, inter_alrm, FALLING);
+        Serial.println("ALARM_PIN pinmode");
+}
+
+void loop() {
+        if (is_alrm) {
+                nowTime = millis(); // Запись текущего времени в миллисекундах,
+                signals = readCommand();
+                Serial.print(signals);
+                Serial.println("\tсигналов");
+                is_alrm = false;
+        }
+}
