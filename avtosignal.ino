9dd0e432571a531d9abd0aa291c75840f74160e8
Tikhon <41113186+TikhonP@users.noreply.github.com>
2020-04-08 00:52:41 +0300

    Переработана логика работы avtosignal с кнопками

--- avtosignal.ino
+++ avtosignal.ino
@@ -1,9 +1,9 @@
 // Для кнопок
 #include "GyverButton.h"
 
-#define BUT1_PIN A1
-#define BUT2_PIN A2
-#define BUT3_PIN A3
+#define BUT1_PIN 7
+#define BUT2_PIN 6
+#define BUT3_PIN 5
 
 GButton butt1(BUT1_PIN);
 GButton butt2(BUT2_PIN);
@@ -47,7 +47,7 @@ DS1302 rtc(kCePin, kIoPin, kSclkPin);
 }
 
 // Для входа сигнализации
-#define ALARM_PIN 5 // Пин подключения сигналов после оптореле
+#define ALARM_PIN 4 // Пин подключения сигналов после оптореле
 #define DELAY_SIGNALS 500 // Задержка между сигналами в миллисекундах
 GButton alrm(ALARM_PIN);
 
@@ -55,12 +55,28 @@ unsigned long nowTime; // Последнее время срабатывания
 int signals;
 
 
+void sound(int signls) {
+        myDFPlayer.pause();
+        delay(50);
+        track_count = myDFPlayer.readFileCountsInFolder(signls);
+        myDFPlayer.playFolder(signls, random(track_count));
+        Serial.print("PLAY track > ");
+        Serial.print(random(track_count));
+        Serial.print("  folder > ");
+        Serial.println(signls);
+        // delay(2000);
+}
+
+
 void setup() {
         Serial.begin(9600); // Вывод
         Serial.println("Out 9600");
 
 
-        alrm.setTickMode(MANUAL)
+        alrm.setTickMode(MANUAL);
+        butt1.setTickMode(MANUAL);
+        butt2.setTickMode(MANUAL);
+        butt3.setTickMode(MANUAL);
 
 
         playerSerial.begin(9600);
@@ -94,129 +110,69 @@ void setup() {
         myDFPlayer.outputDevice(DFPLAYER_DEVICE_SD);
         delay(200);         // даем немного времени,ждем начало проигрывания
         Serial.println("Playing...");
-        int track_count = myDFPlayer.readFileCountsInFolder(1);
-        myDFPlayer.playFolder(1, random(track_count));
+        sound(0);
         delay(5000);
 }
 
 void loop() {
+
         // Проверка входа сигнализации
         alrm.tick();
 
         if (alrm.isHold()) {
-                myDFPlayer.pause();
-                delay(50);
-                track_count = myDFPlayer.readFileCountsInFolder(0);
-                myDFPlayer.playFolder(0, random(track_count));
-                delay(3000);
-                Serial.println("Тревога");
+                sound(0);
+                while (alrm.isHold()) {
+                        alrm.tick();
+                        Serial.println("Тревога");
+                        delay(500);
+                }
         }
-
         else if (alrm.hasClicks()) {
-                signals = butt1.getClicks() + 1;
-                myDFPlayer.pause();
-                delay(50);
-                track_count = myDFPlayer.readFileCountsInFolder(signals);
-                myDFPlayer.playFolder(signals, random(track_count));
-                Serial.print("PLAY track > ");
-                Serial.print("\t");
-                Serial.print(random(track_count));
-                Serial.print("\t");
-                Serial.print("folder > ");
-                Serial.print("\t");
-                Serial.print(signals);
+                signals = alrm.getClicks();
+                sound(signals);
         }
 
-        // Проверка времени
-        Time t = rtc.time();
-        if ((t.hr==switch_vol_1.hr) & (t.min==switch_vol_1.min)) {
+
+        /*
+           // Проверка времени
+           Time t = rtc.time();
+           if ((t.hr==switch_vol_1.hr) & (t.min==switch_vol_1.min)) {
                 volume = 15;
-        }
-        else if ((t.hr==switch_vol_2.hr) & (t.min==switch_vol_2.min)) {
+           }
+           else if ((t.hr==switch_vol_2.hr) & (t.min==switch_vol_2.min)) {
                 volume = 30;
-        }
-
+           }
+         */
 
         // Проверка кнопки
         butt1.tick();
-        butt2.tick();
-        butt3.tick();
-
-        // выключить воспроизведение, если нажата кнопка или отпущенна
-        if (myDFPlayer.readState()) {
-                if ((butt1.isRelease()) || (butt2.isRelease()) || (butt3.isRelease()) || (butt1.isSingle()) || (butt2.isSingle()) || (butt3.isSingle())) {
+        if (butt1.isSingle()) {
+                if (myDFPlayer.readState()) {
                         myDFPlayer.pause();
+                        Serial.println("Paused");
+                } else {
+                        sound(directory_buttons_1);
+
                 }
         }
-        else {
-                // Включить трек, если нажата кнопка
-                if (butt1.isSingle()) {
-                        track_count = myDFPlayer.readFileCountsInFolder(1);
-                        myDFPlayer.playFolder(directory_buttons_1, random(track_count));
-                        Serial.print("PLAY track > ");
-                        Serial.print("\t");
-                        Serial.print(random(track_count));
-                        Serial.print("\t");
-                        Serial.print("folder > ");
-                        Serial.print("\t");
-                        Serial.print(directory_buttons_1);
-                }
-                else if (butt1.isHold()) {
-                        track_count = myDFPlayer.readFileCountsInFolder(1);
-                        myDFPlayer.playFolder(directory_buttons_1, random(track_count));
-                        Serial.print("PLAY track > ");
-                        Serial.print("\t");
-                        Serial.print(random(track_count));
-                        Serial.print("\t");
-                        Serial.print("folder > ");
-                        Serial.print("\t");
-                        Serial.print(directory_buttons_1);
-                }
+        butt2.tick();
+        if (butt2.isSingle()) {
+                if (myDFPlayer.readState()) {
+                        myDFPlayer.pause();
+                        Serial.println("Paused");
+                } else {
+                        sound(directory_buttons_2);
 
-                if (butt2.isSingle()) {
-                        track_count = myDFPlayer.readFileCountsInFolder(2);
-                        myDFPlayer.playFolder(directory_buttons_2, random(track_count));
-                        Serial.print("PLAY track > ");
-                        Serial.print("\t");
-                        Serial.print(random(track_count));
-                        Serial.print("\t");
-                        Serial.print("folder > ");
-                        Serial.print("\t");
-                        Serial.print(directory_buttons_2);
                 }
-                else if (butt2.isHold()) {
-                        track_count = myDFPlayer.readFileCountsInFolder(2);
-                        myDFPlayer.playFolder(directory_buttons_2, random(track_count));
-                        Serial.print("PLAY track > ");
-                        Serial.print("\t");
-                        Serial.print(random(track_count));
-                        Serial.print("\t");
-                        Serial.print("folder > ");
-                        Serial.print("\t");
-                        Serial.print(directory_buttons_2);
-                }
+        }
+        butt3.tick();
+        if (butt2.isSingle()) {
+                if (myDFPlayer.readState()) {
+                        myDFPlayer.pause();
+                        Serial.println("Paused");
+                } else {
+                        sound(directory_buttons_2);
 
-                if (butt3.isSingle()) {
-                        track_count = myDFPlayer.readFileCountsInFolder(3);
-                        myDFPlayer.playFolder(directory_buttons_3, random(track_count));
-                        Serial.print("PLAY track > ");
-                        Serial.print("\t");
-                        Serial.print(random(track_count));
-                        Serial.print("\t");
-                        Serial.print("folder > ");
-                        Serial.print("\t");
-                        Serial.print(directory_buttons_3);
-                }
-                else if (butt3.isHold()) {
-                        track_count = myDFPlayer.readFileCountsInFolder(3);
-                        myDFPlayer.playFolder(directory_buttons_3, random(track_count));
-                        Serial.print("PLAY track > ");
-                        Serial.print("\t");
-                        Serial.print(random(track_count));
-                        Serial.print("\t");
-                        Serial.print("folder > ");
-                        Serial.print("\t");
-                        Serial.print(directory_buttons_3);
                 }
         }
 }
