7089728c5d31e680550fe6f7ad8adf18efb7bd2b
Tikhon <41113186+TikhonP@users.noreply.github.com>
2020-04-12 14:29:11 +0300

    Busy пин

--- avtosignal.ino
+++ avtosignal.ino
@@ -16,134 +16,52 @@ GButton butt3(BUT3_PIN);
 
 #define SS_RX 3
 #define SS_TX 2
-
+#define busy_pin A0
 
 #define directory_buttons_1 50 // Папка с мелодиями кнопок
 #define directory_buttons_2 51
 #define directory_buttons_3 52
 
-bool playingstate = false;
+boolean playingstate;
 int track_count = 1; // количесто треков в каждой папке
 int volume = 15; // Гормкость 0-30
 
 SoftwareSerial playerSerial(SS_RX, SS_TX);  // RX, TX
 DFRobotDFPlayerMini myDFPlayer;
+void checkerr();
 
 
 // Для часов
-#include <DS1302.h>
-
-Time currenttime(2013, 9, 22, 1, 38, 50, Time::kSunday); // Текущее время, если настройка
-#define isSetTime false // Если нужна настройка времени
-
-Time switch_vol_1(2013, 21, 00, 1, 38, 50, Time::kSunday); // Время переключения громкости 1
-Time switch_vol_2(2013, 8, 00, 1, 38, 50, Time::kSunday); // Время переключения громкости 2
-
-namespace {
-const int kCePin   = 8;  // Chip Enable
-const int kIoPin   = 9;  // Input/Output
-const int kSclkPin = 10;  // Serial Clock
-
-DS1302 rtc(kCePin, kIoPin, kSclkPin);
-}
+// #include <DS1302.h>
+//
+// Time currenttime(2013, 9, 22, 1, 38, 50, Time::kSunday); // Текущее время, если настройка
+// #define isSetTime false // Если нужна настройка времени
+//
+// Time switch_vol_1(2013, 21, 00, 1, 38, 50, Time::kSunday); // Время переключения громкости 1
+// Time switch_vol_2(2013, 8, 00, 1, 38, 50, Time::kSunday); // Время переключения громкости 2
+//
+// namespace {
+// const int kCePin   = 8;  // Chip Enable
+// const int kIoPin   = 9;  // Input/Output
+// const int kSclkPin = 10;  // Serial Clock
+//
+// DS1302 rtc(kCePin, kIoPin, kSclkPin);
+// }
 
 // Для входа сигнализации
 #define ALARM_PIN 4 // Пин подключения сигналов после оптореле
-#define DELAY_SIGNALS 500 // Задержка между сигналами в миллисекундах
+// #define DELAY_SIGNALS 500 // Задержка между сигналами в миллисекундах
 GButton alrm(ALARM_PIN);
 
 unsigned long nowTime; // Последнее время срабатывания сигнала
 int signals;
 
-void checkerr(){
-        if (myDFPlayer.available()) {
-                uint8_t type = myDFPlayer.readType();
-                int value = myDFPlayer.read();
-                switch (type) {
-                case TimeOut:
-                        Serial.println(F("Time Out!"));
-                        break;
-                case WrongStack:
-                        Serial.println(F("Stack Wrong!"));
-                        break;
-                case DFPlayerCardInserted:
-                        Serial.println(F("Card Inserted!"));
-                        break;
-                case DFPlayerCardRemoved:
-                        Serial.println(F("Card Removed!"));
-                        break;
-                case DFPlayerCardOnline:
-                        Serial.println(F("Card Online!"));
-                        break;
-                case DFPlayerUSBInserted:
-                        Serial.println("USB Inserted!");
-                        break;
-                case DFPlayerUSBRemoved:
-                        Serial.println("USB Removed!");
-                        break;
-                case DFPlayerPlayFinished:
-                        playingstate = false;
-                        Serial.print(F("Number:"));
-                        Serial.print(value);
-                        Serial.println(F(" Play Finished!"));
-                        break;
-                case DFPlayerError:
-                        Serial.print(F("DFPlayerError:"));
-                        switch (value) {
-                        case Busy:
-                                Serial.println(F("Card not found"));
-                                break;
-                        case Sleeping:
-                                Serial.println(F("Sleeping"));
-                                break;
-                        case SerialWrongStack:
-                                Serial.println(F("Get Wrong Stack"));
-                                break;
-                        case CheckSumNotMatch:
-                                Serial.println(F("Check Sum Not Match"));
-                                break;
-                        case FileIndexOut:
-                                Serial.println(F("File Index Out of Bound"));
-                                break;
-                        case FileMismatch:
-                                Serial.println(F("Cannot Find File"));
-                                break;
-                        case Advertise:
-                                Serial.println(F("In Advertise"));
-                                break;
-                        default:
-                                break;
-                        }
-                        break;
-                default:
-                        break;
-                }
-        }
+void sound(int signls);
 
-}
-
-
-void sound(int signls) {
-        checkerr();
-        myDFPlayer.pause();
-        delay(50);
-        track_count = myDFPlayer.readFileCountsInFolder(signls);
-        delay(50);
-        myDFPlayer.playFolder(signls, random(track_count));
-        playingstate = true;
-        delay(50);
-        Serial.print("PLAY track > ");
-        Serial.print(random(track_count));
-        Serial.print("  folder > ");
-        Serial.println(signls);
-        checkerr();
-        // delay(2000);
-}
 
 void setup() {
         Serial.begin(9600); // Вывод
-        Serial.println("Out 9600");
-
+        Serial.println("Loading...\nStarting serial output 9600");
 
         alrm.setTickMode(MANUAL);
         butt1.setTickMode(MANUAL);
@@ -152,7 +70,7 @@ void setup() {
 
 
         playerSerial.begin(9600);
-        Serial.println("playerSerial begin");
+        Serial.println("Staring playerSerial 9600...");
 
         /*
            Часы
@@ -169,25 +87,26 @@ void setup() {
          */
 
         // Подключение плеера
+        pinMode(busy_pin, INPUT);
         delay(500);
-        if (!myDFPlayer.begin(playerSerial)) { // запуск плеера //инициализац//более 2 секунд
-                Serial.println(F("Ошибка подключения плеера:"));
+        if (!myDFPlayer.begin(playerSerial, false)) {
+                Serial.println(F("Unable to connect pleer!"));
         }
-
         Serial.println("Setting player");
         myDFPlayer.setTimeOut(500);
         myDFPlayer.outputDevice(DFPLAYER_DEVICE_SD);
         myDFPlayer.volume(volume);         // громкость (0~30).
         myDFPlayer.EQ(DFPLAYER_EQ_NORMAL);
         myDFPlayer.outputDevice(DFPLAYER_DEVICE_SD);
-        delay(200);         // даем немного времени,ждем начало проигрывания
-        Serial.println("Playing...");
-        sound(0);
-        delay(5000);
+        // delay(200);         // даем немного времени,ждем начало проигрывания
+        // Serial.println("Playing...");
+        // sound(0);
+        // delay(5000);
 }
 
 void loop() {
         checkerr();
+        playingstate = digitalRead(busy_pin);
         // Проверка входа сигнализации
         alrm.tick();
         if (alrm.isHold()) {
@@ -235,7 +154,7 @@ void loop() {
                 // int st = myDFPlayer.readState();
                 // Serial.print("State is ");
                 // Serial.println(st);
-                if (playingstate) {
+                if (playingstate == LOW) {
                         myDFPlayer.pause();
                         Serial.println("Paused");
                 } else {
@@ -248,7 +167,7 @@ void loop() {
                 // int st = myDFPlayer.readState();
                 // Serial.print("State is ");
                 // Serial.println(st);
-                if (playingstate) {
+                if (playingstate == LOW) {
                         myDFPlayer.pause();
                         Serial.println("Paused");
                 } else {
@@ -261,7 +180,7 @@ void loop() {
                 // int st = myDFPlayer.readState();
                 // Serial.print("State is ");
                 // Serial.println(st);
-                if (playingstate) {
+                if (playingstate == LOW) {
                         myDFPlayer.pause();
                         Serial.println("Paused");
                 } else {
@@ -271,8 +190,94 @@ void loop() {
         }
 }
 
+
+void sound(int signls) {
+        checkerr();
+        myDFPlayer.pause();
+        delay(50);
+        track_count = myDFPlayer.readFileCountsInFolder(signls);
+        delay(50);
+        myDFPlayer.playFolder(signls, random(track_count));
+        // playingstate = true;
+        delay(50);
+        Serial.print("PLAY track > ");
+        Serial.print(random(track_count));
+        Serial.print("  folder > ");
+        Serial.println(signls);
+        checkerr();
+        // delay(2000);
+}
+
+
 // void  checkerr() {
 //         if (myDFPlayer.available()) {
 //                 printDetail(myDFPlayer.readType(), myDFPlayer.read()); //Print the detail message from DFPlayer to handle different errors and states.
 //         }
 // }
+
+void checkerr(){
+        if (myDFPlayer.available()) {
+                uint8_t type = myDFPlayer.readType();
+                int value = myDFPlayer.read();
+                switch (type) {
+                case TimeOut:
+                        Serial.println(F("Time Out!"));
+                        break;
+                case WrongStack:
+                        Serial.println(F("Stack Wrong!"));
+                        break;
+                case DFPlayerCardInserted:
+                        Serial.println(F("Card Inserted!"));
+                        break;
+                case DFPlayerCardRemoved:
+                        Serial.println(F("Card Removed!"));
+                        break;
+                case DFPlayerCardOnline:
+                        Serial.println(F("Card Online!"));
+                        break;
+                case DFPlayerUSBInserted:
+                        Serial.println("USB Inserted!");
+                        break;
+                case DFPlayerUSBRemoved:
+                        Serial.println("USB Removed!");
+                        break;
+                case DFPlayerPlayFinished:
+                        // playingstate = false;
+                        Serial.print(F("Number:"));
+                        Serial.print(value);
+                        Serial.println(F(" Play Finished!"));
+                        break;
+                case DFPlayerError:
+                        Serial.print(F("DFPlayerError:"));
+                        switch (value) {
+                        case Busy:
+                                Serial.println(F("Card not found"));
+                                break;
+                        case Sleeping:
+                                Serial.println(F("Sleeping"));
+                                break;
+                        case SerialWrongStack:
+                                Serial.println(F("Get Wrong Stack"));
+                                break;
+                        case CheckSumNotMatch:
+                                Serial.println(F("Check Sum Not Match"));
+                                break;
+                        case FileIndexOut:
+                                Serial.println(F("File Index Out of Bound"));
+                                break;
+                        case FileMismatch:
+                                Serial.println(F("Cannot Find File"));
+                                break;
+                        case Advertise:
+                                Serial.println(F("In Advertise"));
+                                break;
+                        default:
+                                break;
+                        }
+                        break;
+                default:
+                        break;
+                }
+        }
+
+}
