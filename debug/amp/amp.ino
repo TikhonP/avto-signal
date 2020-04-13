fea8b4b055b724bcdca3dfc65ba0467f6004fbd1
Tikhon <41113186+TikhonP@users.noreply.github.com>
2020-04-13 15:22:50 +0300

    Bugfix дебаг реле

--- debug/amp/amp.ino
+++ debug/amp/amp.ino
@@ -1,15 +1,16 @@
-#define pinn A7
+#define PIN_RELAY A7
 
 
 void setup()
 {
   pinMode(PIN_RELAY, OUTPUT);
   // digitalWrite(PIN_RELAY, HIGH);
-  Serial.Begin(9600);
+  Serial.begin(9600);
+}
 void loop()
 {
   digitalWrite(PIN_RELAY, LOW);
-  Serial.println("LOW")
+  Serial.println("LOW");
   delay(3000);
   digitalWrite(PIN_RELAY, HIGH);
   Serial.println("HIGH");
